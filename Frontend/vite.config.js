10810849c563b8595110ae654ea35055c69845c0
Vivek300705 <vivekkumarsulaniya@gmail.com>
2025-12-26 22:26:57 +0530

    Resolve merge conflicts

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,23 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: true,
-    port: 8080,
-    allowedHosts: [
-      "chemocon-flux.onrender.com"
-    ],
+    allowedHosts: "all",
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
