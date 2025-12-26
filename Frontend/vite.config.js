59ea95ebb0a0be816b74a7c20b69e34382ea0b30
VIVEK KUMAR SULANIYA <135797974+Vivek300705@users.noreply.github.com>
2025-12-26 22:05:54 +0530

    Switch to @vitejs/plugin-react-swc and update config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: true,
     port: 8080,
+    allowedHosts: [
+      "chemocon-flux.onrender.com"
+    ],
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
