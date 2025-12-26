b7a9cb5535aa12c07edc70c97a75563f82766132
Vivek300705 <vivekkumarsulaniya@gmail.com>
2025-12-26 22:15:51 +0530

    Fix case-sensitive folder name for about components

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: true,
     port: 8080,
+    allowedHosts: ["chemocon-flux.onrender.com"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
