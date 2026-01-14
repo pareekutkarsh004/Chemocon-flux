8c01102b6bb200645f17f5c39ccdd9ec8797254c
vansh <vanshsingh2772@gmail.com>
2026-01-15 01:21:02 +0530

    logo changed

--- Frontend/src/components/layout/Header.jsx
+++ Frontend/src/components/layout/Header.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import { cn } from "@/lib/utils";
-import mnnitLogo from "@/assets/Chem-Conflux20_Logo.png";
+import mnnitLogo from "@/assets/Final Logo.png";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import logo from "@/assets/logo.jpg";
 
